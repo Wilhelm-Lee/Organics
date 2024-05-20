2b59d2d519b27def7368cf29ec9c93f56bc2f3e7
William <williampascal_official@outlook.com>
2024-05-20 23:08:59 +0800

    (ADD) DynArray DynEnum DynFunctionPointer DynObject DynPointer DynString DynStruct DynUnion DynVar

---
+++ dynstruct.h
@@ -0,0 +1,12 @@
+#ifndef ORGANICS_DYNSTRUCT_H
+# define ORGANICS_DYNSTRUCT_H
+
+# include <Compound/object.h>
+
+typedef struct {
+  
+} DynStruct;
+
+Report DynStruct_Create()
+
+#endif  /* ORGANICS_DYNSTRUCT_H */
