a952f0291b3388b155f25b3f73d37498daf45977
William <williampascal_official@outlook.com>
2024-05-21 16:07:23 +0800

    (SUB) Added Compound as submodule

---
+++ dynclass.h
@@ -0,0 +1,8 @@
+#ifndef ORGANICS_DYNCLASS_H
+# define ORGANICS_DYNCLASS_H
+
+# include <Compound/class.h>
+
+Status DynClass_ImplementFunction(Class *inst, FunctionPointer *funcptr);
+
+#endif  /* ORGANICS_DYNCLASS_H */
