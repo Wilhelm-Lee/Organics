c687a9c6d624d82e825f946015c83657292510bd
William <williampascal_official@outlook.com>
2024-05-21 16:11:32 +0800

    Revert "(SUB) Added Compound as submodule"
    This reverts commit 3e5ebaa26351740a6e0db2887627e3b39f3883c9.
    
    Unwanted commit message was comitted, now removed.

--- dynclass.h
+++
@@ -1,8 +0,0 @@
-#ifndef ORGANICS_DYNCLASS_H
-# define ORGANICS_DYNCLASS_H
-
-# include <Compound/class.h>
-
-Status DynClass_ImplementFunction(Class *inst, FunctionPointer *funcptr);
-
-#endif  /* ORGANICS_DYNCLASS_H */
